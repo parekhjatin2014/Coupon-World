3c82effd7be5ccc48810a56a960448efb3d61de1
Jatin Parekh <jatin.parekh@globallogic.com>
2015-12-11 14:03:25 +0530

    refactor

--- src/com/example/app/CouponApplication.java
+++ src/com/example/app/CouponApplication.java
@@ -1,12 +1,3 @@
-/*
- * Copyright Medtronic, Inc. 2015
- *
- * MEDTRONIC CONFIDENTIAL: This document is the property of Medtronic,
- * Inc.,and must be accounted for. Information herein is confidential. Do
- * not reproduce it, reveal it to unauthorized persons, or send it outside
- * Medtronic without proper authorization.
- */
-
 package com.example.app;
 
 import android.app.Application;
